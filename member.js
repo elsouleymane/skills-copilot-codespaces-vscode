c111d2161ac33786f69fb549aef6641727829e7a
elsouleymane <el.nuhu@uvci.edu.ci>
2024-12-09 21:55:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/members/views/skills-member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: 'm'
+        }
+
+    };
+}
